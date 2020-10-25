bf1de7625efdd19075fca76a0703270d9b4064ac
Eduardo Corzo <eduardo.corzo@sarsy-srl.com>
2020-10-25 19:48:28 -0300

    WIP - Contexto para cambiar de tema

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, BrowserRouter as Router, Redirect, Switch } from 'react-router-dom';
 import { UserContext } from './context/user.context'
+import { ThemeContext } from './context/theme.context'
 import * as Pages from './pages';
+import ThemeSwitcher from './components/theme/themeSwitch'
 
 export default function App() {
 
   const [user, setUser] = useState();
+
+  useEffect(() => {
+    console.log(user)
+  },[user])
+
   return (
     <Router>
       <Switch>
         {/* <Route exact path="/Login" component={Pages.Login} setUser={() => setUser}/> */}
         <Route path="/Login" render={props => <Pages.Login setUser={() => setUser} />} />
         <UserContext.Provider value={user}>
-          <Route exact path="/Home" component={Pages.Home} />
-          <Route exact path="/Image/:id" component={Pages.ImageDetail} />
+          <ThemeContext.Provider value={"lightTheme"}>
+            <ThemeSwitcher />
+            <Route exact path="/Home" component={Pages.Home} />
+            <Route exact path="/Image/:id" component={Pages.ImageDetail} />
+          </ThemeContext.Provider>
         </UserContext.Provider>
       </Switch>
     </Router>
--- src/context/theme.context.js
+++ src/context/theme.context.js
@@ -1,11 +1,13 @@
 import React, { createContext, useState } from "react";
+import theme from '../assets/js/theme'
 
-export const ThemeContext = createContext();
+export const ThemeContext = createContext("lightTheme");
 
-const ThemeContext = ({ children }) => {
+const ThemeProvider = ({ children }) => {
   const [themeMode, setThemeMode] = useState("lightTheme");
 
   const value = { themeMode };
+  const costumTheme = theme[themeMode];
 
   return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>;
 };
