346987f52b88a06b1f43450e47981fe80be542c2
Eduardo Corzo <eduardo.corzo@sarsy-srl.com>
2020-10-26 19:10:57 -0300

    Se agregan estilos al login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,15 +1,15 @@
-import React, { Fragment , useContext, useState} from 'react';
+import React, { Fragment, useContext, useState } from 'react';
 import { userService } from '../services/user.service'
 import { useForm } from "react-hook-form";
 import { useHistory } from 'react-router-dom';
 import { UserContext } from '../context/user.context'
-import '../assets/css/themes.css'
+import '../assets/css/login.css'
 
 
 export function Login(props) {
     const { register, handleSubmit } = useForm();
     const history = useHistory()
-    const {user, setUser} = useContext(UserContext)
+    const { user, setUser } = useContext(UserContext)
 
     const onSubmit = (data) => {
         let response = userService.login(data);
@@ -25,20 +25,35 @@ export function Login(props) {
 
     return (
         <Fragment>
-            <div>
-                
-                <form onSubmit={handleSubmit(onSubmit)} name="login">
-                    <div>
-                        <input type="text" name="user" ref={register({ required: true })}></input>
-                        <input type="password" name="password" ref={register({ required: true })}></input>
+            <div class="container login_background">
+                <div class="d-flex justify-content-center h-100">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>Login</h3>
+                        </div>
+                        <div class="card-body">
+                        <form onSubmit={handleSubmit(onSubmit)} name="login">
+                                <div class="input-group form-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                    </div>
+                                    <input type="text" name="user" ref={register({ required: true })} className="form-control" ></input>
+                                </div>
+                                <div class="input-group form-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-key"></i></span>
+                                    </div>
+                                    <input type="password" name="password" ref={register({ required: true })} className="form-control"></input>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" value="Login" class="btn float-right login_btn" />
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <div>
-                        <button type="submit">
-                            Iniciar sesion
-                        </button>
-                    </div>
-                </form>
+                </div>
             </div>
+
         </Fragment>
     )
 }
